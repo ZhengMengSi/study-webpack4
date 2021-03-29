0bffe1addef5d189d932450a17a5efe00da4c63d
zms <547151644@qq.com>
2021-03-29 19:17:56 +0800

    1

---
+++ 10.css兼容性处理/webpack.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    'postcss-preset-env'
+                                ]
+                            }
+
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
